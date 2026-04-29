ad670553786be6397d33331e19a1c9ae847bcfc3
KiwiBoi2 <aaronclh@outlook.com>
2026-04-30 09:59:29 +1200

    added comments

--- website/views.py
+++ website/views.py
@@ -1,9 +1,15 @@
+# import external libraries
 from flask import Blueprint, render_template
 
+# set views as blueprint 
 views = Blueprint("views", "__name__")
 
 
+# default/home rout
 @views.route("/")
 @views.route("/home")
+
+# home route function
+# returns homepage
 def home():
     return render_template("home.html")
