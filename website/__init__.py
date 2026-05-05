194df12e4e96e9f13ef31b4b695293abfe5a572e
KiwiBoi2 <aaronclh@outlook.com>
2026-05-05 12:19:07 +1200

    named database, configged secret key for encryption

--- website/__init__.py
+++ website/__init__.py
@@ -4,11 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
+DB_NAME = "database.db"
 
 # create app function
 # returns app
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = ")Ld{<oDfg}7^{@WY[r2y<EsQHay6[TEi"
 
     # import views from views.py
     from .views import views
