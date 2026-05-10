4d9914456319085960bcf815d1ad5456a3081e8f
KiwiBoi2 <aaronclh@outlook.com>
2026-05-11 11:52:45 +1200

    working on create-post route

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
 # import external libraries
 from flask import Blueprint, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 # set views as blueprint 
 views = Blueprint("views", "__name__")
@@ -14,3 +14,11 @@ views = Blueprint("views", "__name__")
 # returns homepage
 def home():
     return render_template("home.html", user=current_user)
+    
+
+# create blog post route
+@views.route("/create-post", methods=["GET", "POST"])
+@login_required
+# create post route function
+# returns create_post page
+def create_post():
