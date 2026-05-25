089e7fa798b8844bd02cd1cf6782649e1c7488a3
KiwiBoi2 <aaronclh@outlook.com>
2026-05-25 12:20:55 +1200

    debugging

--- website/models.py
+++ website/models.py
@@ -27,5 +27,6 @@ class Post(db.Model):
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(200), unique=False, nullable=False)
+    post = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
     date_created = db.Column(db.DateTime(timezone=True), default=func.now())
-    author = db.Column(db.Integer, db.ForeignKey('post.id', ondelete='CASCADE'), nullable=False)
+    author = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'), nullable=False)
